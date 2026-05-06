0f0a9788f436e21503f3425dfbfb8ad097698851
Aamir shehzad <aamirs13370@gmail.com>
2026-05-06 14:25:27 +0500

    Apex Global Variable.js

--- Apex Global Variable.js
+++ Apex Global Variable.js
@@ -38,10 +38,26 @@ apex.server.process(
     }
 );
 
-
-
-
+-----------------------------page loard trriger-----------------------------------------
 
 Reload Full Page in Dynamic Trigger
 
 location.reload();
+
+
+---------------------ig enable/Disable--------------------------------------
+-----------------F12 --action list on YOUR_IG in console-------------------
+
+var ig = apex.region("YOUR_IG_STATIC_ID").widget();
+var actions =ig.interactiveGrid("getActions");
+actions.list();
+
+
+------------row level---------------
+
+var ig = apex.region("YOUR_IG_STATIC_ID").widget();
+var actions = ig.interactiveGrid("getActions");
+
+// Disable actions
+actions.disable("row-duplicate");
+actions.disable("row-delete");
